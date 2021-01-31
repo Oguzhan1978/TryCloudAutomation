04527e237a30c47f63349545ac659e861e30461a
Malik <aamiramallick7@gmail.com>
2021-01-31 01:07:34 -0500

    app setting

---
+++ src/test/java/com/trycloud/tests/UserStories/FilesModule/AppSetting_Aamir.java
@@ -0,0 +1,12 @@
+package com.trycloud.tests.UserStories.FilesModule;
+
+
+//Click Settings on the left bottom corner
+//Verify user can click any buttons.
+
+import com.trycloud.tests.base.TestBase;
+
+public class AppSetting_Aamir extends TestBase {
+
+
+}
