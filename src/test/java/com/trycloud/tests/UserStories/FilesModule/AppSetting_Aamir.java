3d0629c0d87850a5ad50e4f6c32b38af7f82c318
Malik <aamiramallick7@gmail.com>
2021-01-31 14:50:59 -0500

    completed app setting

--- src/test/java/com/trycloud/tests/UserStories/FilesModule/AppSetting_Aamir.java
+++ src/test/java/com/trycloud/tests/UserStories/FilesModule/AppSetting_Aamir.java
@@ -5,8 +5,28 @@ package com.trycloud.tests.UserStories.FilesModule;
 //Verify user can click any buttons.
 
 import com.trycloud.tests.base.TestBase;
+import com.trycloud.utilities.BrowserUtils;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class AppSetting_Aamir extends TestBase {
 
+    @Test
+    public void Tc09_change_the_app_setting() {
+
+        //Finding the setting button and clicking it
+        driver.findElement(By.xpath("//button[normalize-space()='Settings']")).click();
+        BrowserUtils.sleep(5);
+
+        //verifying if all Buttons are clickable
+        boolean FirstCheckBox = driver.findElement(By.xpath("//label[normalize-space(text())='Show rich workspaces']")).isEnabled();
+        boolean SecondCheckBox = driver.findElement(By.xpath("//label[.='Show recommendations']")).isEnabled();
+        boolean ThirdCheckBox = driver.findElement(By.cssSelector("div[id='files-setting-showhidden']>label")).isEnabled();
+
+
+        Assert.assertTrue(FirstCheckBox && SecondCheckBox && ThirdCheckBox);
+
+    }
 
 }
