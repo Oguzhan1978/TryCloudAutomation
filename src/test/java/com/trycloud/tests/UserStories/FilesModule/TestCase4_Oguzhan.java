4eb97053d329e5c0120f83fb73a43bab3ab1daf0
Oguzhan1978 <74024518+Oguzhan1978@users.noreply.github.com>
2021-02-04 15:28:48 -0500

    updated TC04

--- src/test/java/com/trycloud/tests/UserStories/FilesModule/TestCase4_Oguzhan.java
+++ src/test/java/com/trycloud/tests/UserStories/FilesModule/TestCase4_Oguzhan.java
@@ -30,10 +30,15 @@ public class TestCase4_Oguzhan extends TestBase {
         driver.findElement(By.id("submit-form")).click();
         BrowserUtils.sleep(5);
 
+        // clicking file module
+        driver.findElement(By.xpath("//ul[@id='appmenu']//a[@aria-label='Files']//*[local-name()='svg']//*[name()='image' and contains(@class,'app-icon')]")).click();
+        // clicking favorite module
         driver.findElement(By.xpath("//a[@class='nav-icon-favorites svg']")).click();
         BrowserUtils.sleep(3);
+        //clicking (...) button
         driver.findElement(By.xpath("//tbody/tr[1]/td[1]/a[1]/span[3]/a[2]/span[1]")).click();
         BrowserUtils.sleep(3);
+        //clicking remove from favorite.
         driver.findElement(By.xpath("//span[normalize-space()='Remove from favorites']")).click();
         BrowserUtils.sleep(5);
 
