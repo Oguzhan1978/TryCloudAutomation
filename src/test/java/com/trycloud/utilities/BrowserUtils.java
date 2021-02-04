d46ccf8594da9c25feff02498010dc6fec686b1f
Egemen Eliz <egemenelz@gmail.com>
2021-02-04 10:55:25 -0500

    screenshot

--- src/test/java/com/trycloud/utilities/BrowserUtils.java
+++ src/test/java/com/trycloud/utilities/BrowserUtils.java
@@ -5,9 +5,12 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BrowserUtils extends TestBase {
 
@@ -27,6 +30,18 @@ public class BrowserUtils extends TestBase {
             FileUtils.copyFile(screen, new File(path));
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    public static void getElementScreenshot(WebElement element) {
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(" MMMM/ dd, hh.mm a");
+
+
+        File file = element.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File("/Users/egemeneliz/Desktop/ScreenShotsForTest/Element Screenshot Of " + dtf.format(dateTime) + ".png"));
+        } catch (IOException e) {
+
         }
     }
 }
