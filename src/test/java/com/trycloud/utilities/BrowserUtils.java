312100daf0cbc31394dc2309c1c5beb311143f96
Erjon Domi <erjondomi@hotmail.com>
2021-02-04 10:51:46 -0500

    Screen Shot

--- src/test/java/com/trycloud/utilities/BrowserUtils.java
+++ src/test/java/com/trycloud/utilities/BrowserUtils.java
@@ -1,6 +1,15 @@
 package com.trycloud.utilities;
 
-public class BrowserUtils {
+import com.trycloud.tests.base.TestBase;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class BrowserUtils extends TestBase {
 
    public static void sleep(int seconds){
        seconds *= 1000;
@@ -10,4 +19,14 @@ public class BrowserUtils {
            e.printStackTrace();
        }
    }
+
+    public static void takeScreenShot(String path){
+
+        File screen = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screen, new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
